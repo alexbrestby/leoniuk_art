0469adc1ffafa241711f2de9e18c27c2013257ac
Alex Leoniuk <alex.brest.by@gmail.com>
2023-08-23 21:50:28 +0300

    feat: neon logo add

--- src/components/UI/Logo/MyLogo.tsx
+++ src/components/UI/Logo/MyLogo.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Styles from './MyLogo.module.css'
 
 interface customStyle {
@@ -6,12 +7,72 @@ interface customStyle {
 
 const MyLogo = (props: customStyle) => {
 
+  useEffect(() => {
+    const elements = [
+      { id: '.first_S', classes: ['anime',], delay: 1000 },
+      { id: '.first_T', classes: ['anime'], delay: 1500 },
+      { id: '.first_S', classes: ['d-none'], delay: 5000 },
+      { id: '.first_T', classes: ['d-none'], delay: 5500 },
+      { id: '.first_A', classes: ['anime'], delay: 2000 },
+      { id: '.second_T', classes: ['anime'], delay: 6000 },
+      { id: '.first_R', classes: ['anime'], delay: 10500 },
+      { id: '.second_T', classes: ['anime'], delay: 10500 },
+      { id: '.second_S', classes: ['anime'], delay: 10500 },
+    ];
+
+    elements.forEach((element) => {
+      const { id, classes, delay } = element;
+
+      setTimeout(() => {
+        const element = document.querySelector(id);
+
+        if (element) {
+          classes.forEach((className) => {
+            element.classList.add(className);
+            if (element.classList.contains('hidden')) {
+              element.classList.remove('hidden');
+            }
+          });
+        }
+      }, delay);
+
+      setTimeout(() => {
+        const element = document.querySelector(id);
+
+        if (element) {
+          classes.forEach((className) => {
+            element.classList.add(className);
+            if (element.classList.contains('anime')) {
+              element.classList.remove('anime');
+            }
+          });
+        }
+      }, 12000);
+    });
+  }, []);
+
+
   const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)")
 
   return (
     <a href='/' className={Styles.MyLogo} style={props}>
       <div className={!darkThemeMq.matches ? `${Styles.LogoImg} ${Styles.LogoImg_dark}` : Styles.LogoImg}></div>
-      <h3>leoniuk_art</h3>
+      {props.fontSize === '2em' ?
+        (<h3> leoniuk_
+          <span className="first_S">s</span>
+          <span className="first_T">t</span>
+          <span className="first_A">a</span>
+          <span className="first_R">r</span>
+          <span className="second_T hidden">t</span>
+          <span className="second_S hidden">s</span>
+        </h3>)
+        : (<h3>leoniuk_
+          <span>a</span>
+          <span>r</span>
+          <span>t</span>
+          <span>s</span>
+        </h3>)}
+
     </a>
   )
 }
